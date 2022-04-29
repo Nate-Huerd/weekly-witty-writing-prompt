6f41d8a77787dbf69055656c4781402e4c71b54a
Nate-Huerd <nhuerd3@gmail.com>
2022-04-28 20:27:08 -0500

    created pages folder and started Home.js

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Home should show Prompt and user Stories
+// need pages set up for Stories,
+const Home = () => {
+    return (
+        <div className='container'>
+            
+        </div>
+    );
+};
+
+export default Home;
