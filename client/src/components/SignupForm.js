064e734fd1d798d10370cb1967013d4d9d1c6aff
Kawilder <kevin_wilde564@yahoo.com>
2022-04-28 19:05:40 -0500

    removed 2 lines that arnt needed rn

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
