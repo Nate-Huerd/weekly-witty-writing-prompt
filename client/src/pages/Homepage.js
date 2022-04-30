241301f6dcd454b5132362eed7f44c71fcde2ac0
Nate-Huerd <nhuerd3@gmail.com>
2022-04-29 22:47:17 -0500

    added imports to homepage and added addtional components

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,13 +1,16 @@
 import React from 'react';
-// import other components
-
+import Prompts from "../components/Prompts";
+import Stories from "../components/Stories";
+import Comments from "../components/Comments";
 
 // Home should show Prompt and user Stories
 // need pages set up for Stories,
 const Home = () => {
     return (
         <div className='container'>
-            
+            <Prompts />
+            <Stories />
+            <Comments />
         </div>
     );
 };
