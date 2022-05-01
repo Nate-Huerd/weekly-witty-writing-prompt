56827474b1f380a131c5c0938b2c4c016e5dc173
Thiccie-C <Nelsonnoremac@gmail.com>
2022-05-01 12:51:43 -0500

    making sure all files are added

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,44 @@ mutation AddStory($author: String!, $storyText: String!) {
       createdAt
     }
   }`
+export const SIGN_UP = gql`
+mutation signUp($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      _id
+      username
+      email
+    }
+  }`
+export const LOGIN = gql`
+utation Login {
+    login {
+      token
+      user {
+       _id
+       username 
+      }
+    }
+  }`
+export const ADD_COMMENT = gql `
+mutation addComment($author: String!, $commentText: String!, $storyId: ID!) {
+    addComment(author: $author, commentText: $commentText, storyId: $storyId) {
+      _id
+      commentText
+      author {
+       username 
+      }
+      createdAt
+    }
+  }`
+export const DELETE_COMMENT = gql`
+mutation DeleteComment($commentId: ID!, $storyId: ID!) {
+    deleteComment(commentId: $commentId, storyId: $storyId)
+  }`
+export const DELETE_STORIES_BY_AUTHOR = gql`
+mutation deleteStoryByAuthor($author: String!) {
+    deleteStoryByAuthor(author: $author)
+  }`
+export const DELETE_STORY_BY_ID = gql`
+mutation deleteStoryById($storyId: String!) {
+    deleteStoryById(storyId: $storyId)
+  }`
