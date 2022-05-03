2530c4454a3d76e45085112fdcca3e62f0cdab45
Nate-Huerd <nhuerd3@gmail.com>
2022-05-02 20:39:18 -0500

    removed spacing

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,4 @@
 const { Comment, Story, User, Prompt } = require('../models')
-
-
 const {AuthenticationError} = require('apollo-server-express')
 const {signToken} = require('../utils/auth')
 
@@ -54,17 +52,13 @@ const resolvers = {
     Mutation: {
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
-            
             if (!user) {
               throw new AuthenticationError('Incorrect credentials');
             }
-      
             const correctPw = await user.isCorrectPassword(password);
-      
             if (!correctPw) {
               throw new AuthenticationError('Incorrect credentials');
             }
-      
             const token = signToken(user);
             return { token, user };
         },
@@ -79,17 +73,13 @@ const resolvers = {
         },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
-      
             if (!user) {
               throw new AuthenticationError('Incorrect credentials');
             }
-      
             const correctPw = await user.isCorrectPassword(password);
-      
             if (!correctPw) {
               throw new AuthenticationError('Incorrect credentials');
             }
-      
             const token = signToken(user);
             return { token, user };
           },
