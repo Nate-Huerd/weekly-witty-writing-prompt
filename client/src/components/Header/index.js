11e6cd9905cbf001614753c7bb281dcf784810ae
Cameron Nelson <nelsonnoremac@gmail.com>
2022-05-03 02:22:08 -0500

    added story from, and did some refactoring in the header and made Header change based on the page.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import Auth from '../../utils/auth';
@@ -8,26 +8,52 @@ const Header = () => {
     event.preventDefault();
     Auth.logout();
   };
-
+  const [currentPage, setCurrentPage] = useState('')
+  window.onload = () => {
+    const currentPageName = window.location.href.split('/')[3]
+    setCurrentPage(currentPageName)
+  }
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">
+        <Link to="/" onClick={() => setCurrentPage('')}>
           <h1>Weekly Witty Writing Prompts</h1>
         </Link>
 
         <nav className="text-center">
           {Auth.loggedIn() ? (
             <>
-              <Link to="/Dashboard">Dashboard</Link>
+            {currentPage ==="Dashboard" ? 
+            '':<Link to="/Dashboard" onClick={() => setCurrentPage('Dashboard')}>Dashboard</Link>
+            }
+              
               <a href="/" onClick={logout}>
                 Logout
               </a>
             </>
           ) : (
             <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
+            {
+              currentPage === '' ?
+              <>
+              <Link to="/login" onClick={() => setCurrentPage('login')}>Login</Link> <Link to="/signup" onClick={() => setCurrentPage('signup')}>Signup</Link> 
+              </>: ''
+            }
+            {
+              currentPage === 'login' ?
+              <>
+              Don't have an account please:
+              <Link to="/signup" onClick={() => setCurrentPage('signup')}> Signup</Link>  
+              </>: ''
+            }
+            {
+              currentPage === 'signup' ?
+              <>
+              Have an account 
+              <Link to="/login"onClick={() => setCurrentPage('login')}> Login </Link> 
+              here
+              </>: ''
+            }
             </>
           )}
         </nav>
