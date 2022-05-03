72cae3c0f53199c6fbfeb74b1fc9e77f08da84f4
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-05-03 11:49:13 -0500

    added no match if the route path doesnt match

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
-import Homepage from './pages/Homepage';
-import Login from './pages/Login';
-// import NoMatch from './pages/NoMatch';
-import Signup from './pages/Signup';
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import NoMatch from './pages/NoMatch';
+import Signup from "./pages/Signup";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-
-
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const client = new ApolloClient({
@@ -32,22 +34,12 @@ function App() {
           <Header />
           <div className="container">
             <Routes>
-              <Route
-                path="/"
-                element={<Homepage />}
-              />
-              <Route
-                path="/login"
-                element={<Login />}
-              />
-              <Route
-                path="/signup"
-                element={<Signup />}
-              />
-              <Route
-                path="/dashboard"
-                element={<Dashboard />}
-              />
+              <Route path="/" element={<Homepage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+
+              <Route path="*" element={<NoMatch />} />
             </Routes>
           </div>
           <Footer />
