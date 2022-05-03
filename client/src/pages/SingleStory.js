0dd59337ce6edeee83e8fdc51d4833ce427489b3
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-05-03 12:26:34 -0500

    added single story page, made comment component and put it in story., The query for story isn't working correctly' '

--- client/src/pages/SingleStory.js
+++ client/src/pages/SingleStory.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_STORY } from '../utils/queries';
+import CommentList from '../components/CommentList';
 
 const SingleStory = props => {
+
+    const { id: storyId } = useParams();
+    console.log(storyId);
+    const { loading, data } = useQuery(QUERY_STORY, {
+        variables: { id: storyId }
+      });
+      console.log(data); //it doesn't like this
+      const story = data?.Story || {};
+      
+      if (loading) {
+        return <div>Loading...</div>;
+      }
+
     return (
       <div>
         <div className="card mb-3">
           <p className="card-header">
             <span style={{ fontWeight: 700 }} className="text-light">
-              Username
+              {story.author}
             </span>{' '}
-            thought on createdAt
+             {story.createdAt}
           </p>
           <div className="card-body">
-            <p>Thought Text</p>
+            <p>{story.storyText}</p>
           </div>
         </div>
+
+        {story.commentCount > 0 && <CommentList comments={story.comments} />}
       </div>
     );
   };
